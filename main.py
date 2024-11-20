b810cc8e72f3e222c991b73de7f3140bae7185c2
Nicolas <nicolaskjunges@gmail.com>
2024-11-19 21:22:21 -0300

    Definindo a classe AviaoLocalizacao

--- main.py
+++ main.py
@@ -34,9 +34,14 @@ pygame.display.set_caption('River Raid')
 tela = pygame.display.set_mode(tamanho_tela)
 tela.fill(cor_branco)
 
-#aviao_localizacao = AviaoLocalizacao(155, 125)
-imagem_aviao = pygame.image.load('aviao.png').convert_alpha()
+# Definindo a classe AviaoLocalizacao
+class AviaoLocalizacao:
+    def __init__(self, x, y):
+        self.x = x  # Atributo x
+        self.y = y  # Atributo y
 
+aviao_localizacao = AviaoLocalizacao(155, 125)
+imagem_aviao = pygame.image.load('aviao.png').convert_alpha()
 tela.blit(imagem_aviao, (aviao_localizacao.x, aviao_localizacao.y))
 
 pygame.display.update()
