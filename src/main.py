31faaa4232d5a64a562aa9247a72478e12860f49
Rogerio Batisti Junior <rogeriobatistijunior@Batistis-MacBook.local>
2024-11-20 20:28:01 -0300

    commit da noite

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import os.path
+import sys
+
 import pygame
 import time
 from entities.aviao import Aviao
@@ -30,10 +32,43 @@ tela = pygame.display.set_mode(tamanho_tela)
 tela.fill(cor_branco)
 
 aviao = Aviao()
+
 imagem_path = os.path.join(os.getcwd(), '..', 'assets', 'images', 'aviao.png')
 
 imagem_aviao = pygame.image.load(imagem_path).convert_alpha()
 tela.blit(imagem_aviao, (aviao.x, aviao.y))
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Captura o estado das teclas
+    teclas = pygame.key.get_pressed()
+
+    if teclas[pygame.K_RIGHT]:
+        aviao.x += 3
+
+    if teclas[pygame.K_LEFT]:
+        aviao.x -= 3
+
+    if teclas[pygame.K_UP]:
+        aviao.y -= 3
+
+    if teclas[pygame.K_DOWN]:
+        aviao.y += 3
+
+    # Deixando a tela em branco novamente
+    tela.fill(cor_branco)
+
+    tela.blit(imagem_aviao, (aviao.x, aviao.y))
+    pygame.display.update()
+
+    # Controlando o FPS
+
+    # Controlando o FPS (frames por segundo)
+    pygame.time.Clock().tick(60)
 
 pygame.display.update()
 
